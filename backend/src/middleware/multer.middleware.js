7df644ee37ce312da510f8e3c7d8ee5ed17134a9
Neev213 <sonineev18@gmail.com>
2026-07-23 13:33:14 +0530

    feat: add multer middleware for handling file uploads with validation and storage configuration

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,13 +1,9 @@
 import multer from "multer";
 import path from "path";
-import fs from "fs";
+import os from "os";
 import { ApiError } from "../utils/ApiError.js";
 
-const uploadDir = "./public/temp";
-
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
+const uploadDir = os.tmpdir();
 
 const storage = multer.diskStorage({
     destination: function (_req, _file, cb) {
